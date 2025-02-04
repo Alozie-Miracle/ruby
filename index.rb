234156a47ca24b16eaa3d74f1b53f6709638f75d
Alozie Miracle Patrick <nacotech09@gmail.com>
2025-02-04 15:16:32 +0100

    check for the case the selected product type is goods, check for the buyers location, if it's spain return spainish VAT, else check if the buyer is from an EU country, if yes and the buyer is an individual return the local vat for that location else consider the person as a company and return reverse charge, if the buyer is not from the the EU country, return Export (No VAT)

--- index.rb
+++ index.rb
@@ -36,17 +36,32 @@ class TaxCalculator
     case product_type
     when 'good'
       # the case the product type is goods, we check for the buyers location and buyer type either individual or company
-
+      goods(buyer_location, buyer_type)
     when 'service'
       # the case the product_type is service, we check if it's an onsite service or digital service
+      if service_location 
+        onsite_service(service_location)
+      else
+        digital_service(buyer_location, buyer_type)
+      end
     else
       "Invalid product type"
     end
   end
 
+  # setting goods, digital_service & onsite_service method to private, because they are only used by the calculate tax method and explicitly needed outside.
+  private
 
+  # in the case the selected product type is goods, check for the buyers location, if it's spain return spainish VAT, else check if the buyer is from an EU country, if yes and the buyer is an individual return the local vat for that location else consider the person as a company
+  # and return reverse charge, if the buyer is not from the the EU country, return Export (No VAT) 
   def goods(buyer_location, buyer_type)
-
+    if buyer_location == "Spain"
+      SPANISH_VAT
+    elsif EU_COUNTRIES.include?(buyer_location)
+      buyer_type == "individual" ? "Local VAT (#{buyer_location})" : "Reverse Charge"
+    else
+      "Export (No VAT)"
+    end
   end
 
 
